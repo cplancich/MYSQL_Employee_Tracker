aec659a0d9d1d131bc835b2516e8cf39d5ff586b
colinplancich <colin.plancich@gmail.com>
2022-08-23 10:10:54 -0700

    updated index.js and schema.sql, added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO employee (first_name, last_name)
+    VALUES ("Jomarion", "Jomaroquai");
--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root', 
-        password: '',
-        database: ''
+        password: 'password',
+        database: 'employees_db'
     },
 );
 
@@ -18,12 +18,14 @@ db.query = utils.promisify(db.query);
     // SELECT * FROM department
 
 // View all roles
-    // SELECT * FROM role
+    // SELECT * FROM roles
 
 // View all employees
     // SELECT * FROM employee
 
+// Create a new department
 // prompt the user for the name of the department
+
     // THEN Run the query
     // INSERT INTO department (name)
     // VALUES (Sales);
@@ -31,11 +33,31 @@ db.query = utils.promisify(db.query);
         // THEN ask the user what they want to do next
 
 // Create a new role
-    // Get the existing departments from the 'department' table
-
+const createRole = async () => {
+// Get the existing departments from the 'department' table
+    const role = await db.query ("SELECT * FROM roles");
     // THEN prompt the user for "title" "salary" and "department" for the role
-        // THEN run the query
-        // INSERT INTO role (title, salary, department_id)
-        // VALUES (?, ?, ?);
+    console.log(role);
 
-        // THEN ask the user what they want to do next
+
+    const answers = inquirer.createPromptModule([
+        // THEN run the query
+        {
+            message: "",
+            name: "",
+            type: "",
+        }
+    ])
+    // INSERT INTO role (title, salary, department_id)
+    // VALUES (?, ?, ?);
+    
+    // THEN ask the user what they want to do next
+}
+
+// ADD Employee
+
+// UPDATE Employee Role
+
+// QUIT Node App
+
+createRole();
